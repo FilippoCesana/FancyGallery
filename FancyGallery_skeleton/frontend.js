21b7d720e15a029cc29948dba693413f3b37674d
Nicola Brunner <brunnn@usi.ch>
2019-12-09 10:25:27 +0100

    Upfated frontend.js

--- FancyGallery_skeleton/frontend.js
+++ FancyGallery_skeleton/frontend.js
@@ -12,23 +12,15 @@ search_input.addEventListener("keyup", (e) => {
 var photographersAtParty = [];
 
 // da inizializzare ogni volta che creo una nuova galleria
-function new_photographer_list(id_evento, id_organizzatore){
-  photographersAtParty = [id_organizzatore];
+function new_photographer_list(id_evento, organizzatore){
+  photographersAtParty = [organizzatore];
 }
 
-function add_photographer(id_evento, id_photographer) {
+function add_photographer(id_evento, photographer) {
   photographersAtParty.push(id_photographer);
 }
 
-function remove_photographer(id_evento, id_photographer) {
-    var newname = document.querySelector('[name=name'+id+']').value;
-    console.log(newname);
-    document.querySelector('[name=name'+id+']').value = newname;
-    var body = {name : newname}
-    doJSONRequest('PUT', "favorites/" + id, {}, body);
+function remove_photographer(id_evento, photographer) {
+  let target = photographersAtParty.findIndex(e => (photographer.id == e._id));
+  favorites.splice(target,1);
 }
-
-
-
-let target = favorites.findIndex(e => (filter.id == e._id));
-favorites.splice(target,1);
