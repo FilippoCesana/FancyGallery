11722acb799f6031cc1553895d21dddac452289f
Jacopo Stucchi <stuccj@usi.ch>
2019-12-09 10:09:00 +0100

    homepage with show more and search

--- FancyGallery_skeleton/public/entry_points/homepage_start.js
+++ FancyGallery_skeleton/public/entry_points/homepage_start.js
@@ -1,3 +1,13 @@
+//function to count times show more button is pressed
+function nextNumber(){
+    if(typeof nextNumber.counter == 'undefined'){
+        nextNumber.counter = 0 ;
+    }
+   return  ++nextNumber.counter;
+}
+
+    
+
 
 function start(){
     
@@ -7,7 +17,7 @@ function start(){
 
     //signin button listen click
     const sign_in_btn = document.getElementById("sign_in_btn");
-    sign_in_btn.addEventListener('click', (e)=>sign_up(e));
+    sign_in_btn.addEventListener('click', (e)=>signUp(e));
 
     //search button listen click
     const search_btn = document.getElementById('search_btn');
@@ -22,33 +32,104 @@ function start(){
     const event_boxes = document.querySelectorAll(".event_box");
     event_boxes.forEach(box=>{
         box.addEventListener('click', (e)=>showEvent(e,box))
-    })
-    
+    });
+
+   
 }
 
 function login(){
-    //TODO:
-    window.location.href = "http://localhost:3000/user/login";
-}
-function sign_up(){
-    window.location.href = "http://localhost:3000/user/signin";
+    //inviare la password in modo sicuro bisogna fare lo start.js del login
+    window.location.href = "http://localhost:3000/user/login"
 }
 
-async function searchEventByName(){
-    const model = 
+function signUp(){
+    window.location.href = "http://localhost:3000/user/signin"
 }
 
-function showMore(){
-    // counter per capire quante volte a richiesto lo showMore e caricare solo nuovi event
-    //richiesta
+function searchEventByName(e){
+   const value = document.getElementById('search_text_field').value.replace(" ","_");
+
+   const options = {
+    method : 'get',
+    headers : {
+        accept : "application/json"
+        }
+    } 
+
+    const url = "http://localhost:3000/event/search&event_name="+value;
+
+    fetch(url,options)
+        .then(res=>res.json().then(model=>{
+            nextNumber.counter = undefined;
+            dust.render("partials/event",{model},(err,out)=>{
+                if(err){throw err}
+                document.getElementById('gallery').innerHTML = out;
+            })
+        }))
+        .catch(err=>{
+            console.log("error during searching for event");
+            throw err;
+        })
+
+}
+
+function showLess(){
+  document.getElementById("show_more_btn").style.display = 'none';
+}
+
+async function showMore(e){
+
+    const options = {
+        method : 'get',
+        headers : {
+            accept : "application/json"
+        }
+    }
+      const n = nextNumber();
+        const url ='http://localhost:3000/event/more&n='+n;
+       fetch(url,options)
+            .then((r)=>r.json().then((model)=>{
+                if(model.length ==0){showLess(); return};
+                dust.render('partials/event',{model},(err,out)=>{
+                    if(err){throw err};
+                    const gallery = document.getElementById('gallery');
+                    const before =  gallery.innerHTML;
+                    gallery.innerHTML = before + " " + out;
+                })
+            }))
+            .catch(err=>{throw err})
+
+
+}
+
+function showEvent(e,item){
+    //capire che event ha cliccato leggendo l'id associato;
+    //richiesta per l'evento 
     //render nel div corretto
-}
 
-function showEvent(){
-    console.log("ricevuto");
+    //TODO:
+    // if already open_event_box do nothing
+    //await chimata fetch 
+    //close button
+    console.log(item)
+    const x = e.clientX;
+    const y = e.clientY;
+   document.body.style.overflow = 'hidden'
+    const open_event_box = document.getElementById("open_event_box");
+    open_event_box.className = "open_event_box_style"
+    open_event_box.style.position = "fixed";
+    open_event_box.style.top      =  100   + 'px';
+    open_event_box.style.left     = 200 + 'px'
+
+    
+    console.log("TODO showEvent");
 }
 
 setTimeout(()=>{
-    start();
-},100);
+   
+
+
+            start();
+   
+},10)
 
