166a5f214b2400c60d738f6f3b6abaa54b5f26bc
Jacopo Stucchi <stuccj@usi.ch>
2019-12-09 10:02:25 +0100

    retrieve events

--- FancyGallery_skeleton/routers/root/root_controller.js
+++ FancyGallery_skeleton/routers/root/root_controller.js
@@ -1,26 +1,53 @@
 const modelManager = require('../../managers/modelManager.js');
 const log = require("debug")(':-> root_controller: ')
 
+const modelManager = require('../../managers/modelManager.js');
+const log = require("debug")(':-> root_controller: ');
+const Event = require("./../../actualSchema/Event");
+
+
 async function sendHomePage(req,res){
 
     log("Sending homepage")
-    try{
-        if(req.accepts("html")){
-            const model = await modelManager.getModel("homepage"); //TODO: connetere il modelManager con il database ora serve contenuti statici
-            res.status(200);
-            res.render('homepage', {model});
-         }else{
-            res.status(200).json(model);
-         }
 
-    }catch(err){
-        log("Error while rendering homepage");
-        res.status(500);
-        res.send("Error");
-        throw err;        
-    }     
+    Event.find({})
+             .then(res=>{
+                 const model = {
+                     event_list = []
+                 }
+                 let event_model = {
+                     name       : undefined,
+                     timestamp  : undefined,
+                     id         : undefined,
+                    dataURL     : undefined,
+                    place       : undefined
+                 }
+
+                 res.forEach(event=>{
+                    event_model.name = event.name;
+                    event_model.id   = event._id;
+                    event_model.timestamp = event.start;
+                    event_model.dataURL   = event.cover;
+                    model.event_list.push(event_model);
+                 });
+
+                 
+                 if(req.accepts("html")){
+                    res.status(200).render("homepage", {model}).catch(err=>{throw err});
+                 }else{
+                    res.status(200).json(model);
+                 }
+
+
+             })
+             .catch(err=>{
+                log("Error while rendering homepage");
+                res.status(500);
+                res.send("Error");
+                throw err;        
+             })
+          
 }
-
 
 
 module.exports.sendHomePage = sendHomePage;
