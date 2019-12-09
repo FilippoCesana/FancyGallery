8c077926fef3575f161f770ee9678add6f473bae
Jacopo Stucchi <stuccj@usi.ch>
2019-12-09 10:46:32 +0100

    added slice

--- FancyGallery_skeleton/routers/root/root_controller.js
+++ FancyGallery_skeleton/routers/root/root_controller.js
@@ -29,7 +29,7 @@ async function sendHomePage(req,res){
                     model.event_list.push(event_model);
                  });
 
-                 
+                 model.event_list = model.event_list.slice(0,3);
                  if(req.accepts("html")){
                     res.status(200).render("homepage", {model}).catch(err=>{throw err});
                  }else{
