9c5ecf77b4b415b77d35fcd55ee628596852c882
Nicola Brunner <brunnn@usi.ch>
2019-12-11 15:20:10 +0100

    Immagine upload Form Fixato
    ORA funziona finalmente!!!!

--- FancyGallery_skeleton/public/entry_points/uploadimagepreview.js
+++ FancyGallery_skeleton/public/entry_points/uploadimagepreview.js
@@ -1,73 +1,43 @@
-// // var image;
-// //
-// // document.getElementById("imageUpload").onchange = function(event) {
-// //   image = new Image();
-// //   image.onload = applyWatermark;
-// //   image.onerror = failedImage;
-// //   image.src = URL.createObjectURL(this.files[0]);
-// // };
-// //
-// // function applyWatermark(){
-// // watermark([image, 'http://localhost:3000/images/fancygallery.png'])
-// //   .image(watermark.image.lowerLeft(0.5))
-// //   .then(img => document.getElementById('container').appendChild(img));
-// // }
-// //
-// // function drawImage(img) {
-// //   var canvas = document.getElementById('Mycanvas');
-// //   var ctx = canvas.getContext('2d');
-// //   canvas.width = this.width;
-// //   canvas.height = this.height;
-// //   ctx.drawImage(this, 0,0);
-// // }
-// //
-// // function failedImage() {
-// //   console.error("File not loaded!");
-// // }
+// var image;
 //
 // document.getElementById("imageUpload").onchange = function(event) {
 //   image = new Image();
-//   image.onload = drawImageNew;
-//   image.src = URL.createObjectURL(this.files[0]);
+//   image.onload = applyWatermark;
 //   image.onerror = failedImage;
+//   image.src = URL.createObjectURL(this.files[0]);
 // };
 //
+// function applyWatermark(e){
+//   watermark([this, '/images/fancygallery.png'])
+//     .image(watermark.image.lowerLeft(0.5))
+//     .then(drawImage);
+// }
 //
-// // setto canvas
-// var canvas = document.getElementById('canvas');
-// var ctx = canvas.getContext('2d');
-//
-// // upload prima immagine
-// var image = new Image();
-// image.onload = drawImageNew;
-// image.src = "galleriafoto/hasli1.jpg";
-//
-// // upload watermark
-// var watermark = new Image();
-// watermark.src = "galleriafoto/filigrana.png";
-//
-// function drawImageNew() {
-//   // disegna prima immagine
-//   ctx.drawImage(image, 0, 0, 300, 150);
-//   // mette trasparenza a 50%
-//   ctx.globalAlpha = 0.5
-//   // disegna watermark
-//   ctx.drawImage(watermark, 0, 0, 300, 150);
+// function drawImage(img) {
+//   var canvas = document.getElementById('canvas');
+//   var ctx = canvas.getContext('2d');
+//   canvas.width = img.width;
+//   canvas.height = img.height;
+//   ctx.drawImage(img, 0,0);
 // }
 //
 // function failedImage() {
 //   console.error("File not loaded!");
 // }
 
-var watermark = new Image();
-watermark.src = "/images/fancygallery.png";
+//
+//-----------------------------------------------------------------------------
+//
+var image;
+var watermark;
 
-//FUNZIONA!!!
 document.getElementById("imageUpload").onchange = function(event) {
-  var image = new Image();
+  image = new Image();
   image.onload = drawImage;
   image.onerror = failedImage;
   image.src = URL.createObjectURL(this.files[0]);
+  watermarkn = new Image();
+  watermarkn.src = "/images/fancygallery.png";
 };
 
 function drawImage() {
@@ -75,9 +45,13 @@ function drawImage() {
   var ctx = canvas.getContext('2d');
   canvas.width = this.width;
   canvas.height = this.height;
+  var n = this.width/3;
+  var m = (n/watermarkn.width)*watermarkn.height;
+  var px = this.width/20;
+  var py = this.height-px-m;
   ctx.drawImage(this, 0,0);
   ctx.globalAlpha = 0.5;
-  ctx.drawImage(watermark, 0,0);
+  ctx.drawImage(watermarkn, px, py, n, m);
 }
 
 function failedImage() {
