a86b349c00f5ec72445da02d12823a2fe04d98b1
Canzo <canzoniere70@gmail.com>
2019-12-11 16:46:21 +0100

    second commit

--- FancyGallery_skeleton/public/entry_points/newevent.js
+++ FancyGallery_skeleton/public/entry_points/newevent.js
@@ -1,59 +1,51 @@
-// function initNewEvent() {
-//   myEvent = {
-//     orginainizer: undefined,
-//     photographers: []
-//   };
-// }
+// TODO: 1) SERCH form,  2) ADD photographer, 3) DELETE photographer
 
-// variables
-let myEvent = {
-  orginainizer: undefined,
-  photographers: []
-};
+document.addEventListener("DOMContentLoaded", start);
 
-var new_list = document.querySelector("div[class = 'container_form']");
-var add_phot = document.querySelector("input[class='bt_add_photographer']");
-var search_input = document.querySelector(
-  "input[name='photographer_search_field']"
-);
-//all these varibles are null
+function start() {
+  // variables
+  let myEvent = {
+    orginainizer: undefined,
+    photographers: []
+  };
 
-// Event Listeners
-// the event listener for remove photographer is already in the botton tag
-console.log(new_list);
-//search_input.addEventListener("keyup", photographer_search);
-function photographer_search() {
-  //photographer search event Listener
-  var s = photographer_search_field.value;
-  console.log(s);
-  doFetchRequest(
-    "GET",
-    "user/search?name=" + s,
-    { "Content-Type": "text/html" },
-    undefined
-  )
-    .then(res => {
-      return res.text();
+  let search_input = document.getElementById("search_input");
+
+  //var new_list = document.querySelector("div[class = 'container_form']");
+  //var add_phot = document.querySelector("input[class='bt_add_photographer']");
+  //all these varibles are null
+
+  // Event Listeners
+  search_input.addEventListener("keypress", photographer_search);
+  // console.log("search_input: ", search_input.getElementsByClassName());
+
+  function photographer_search() {
+    doFetchRequest("GET", "user/search?name=" + search_input.value, {
+      "Content-Type": "text/html"
     })
-    .then(body => {
-      document.getElementById("search_result").innerHTML = body;
-    });
-}
+      .then(res => {
+        console.log(res);
+        //return res.text();
+      })
+      .then(body => {
+        document.getElementById("search_result").innerHTML = body;
+      });
+  }
 
-// search_input.addEventListener("load",);
+  // search_input.addEventListener("load",);
 
-// da inizializzare ogni volta che creo una nuova galleria
-// when page is reloaded {loggedId,}
-function new_photographer_list(organizzatore) {
-  myEvent.organizer = organizzatore;
-  return myEvent.photographers.push(organizzatore);
-}
+  // da inizializzare ogni volta che creo una nuova galleria
+  // when page is reloaded {loggedId,}
+  function new_photographer_list(myEvent) {
+    return myEvent.photographers.push(myEvent.organizer);
+  }
 
-function add_photographer(photographer) {
-  return myEvent.photographers.push(photographer.id);
-}
+  function add_photographer(myEvent, photographer) {
+    return myEvent.photographers.push(photographer.id);
+  }
 
-function remove_photographer(photographer) {
-  let target = myEvent.photographers.findIndex(e => photographer.id == e._id);
-  return myEvent.photographers.splice(target, 1);
+  function remove_photographer(myEvent, photographer) {
+    let target = myEvent.photographers.findIndex(e => photographer.id == e._id);
+    return myEvent.photographers.splice(target, 1);
+  }
 }
