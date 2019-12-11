098e38650124b6dae7901e90c25b404f5fdcaf3b
Canzo <canzoniere70@gmail.com>
2019-12-11 11:44:38 +0100

    first commit, fixing event listeners

--- FancyGallery_skeleton/public/entry_points/newevent.js
+++ FancyGallery_skeleton/public/entry_points/newevent.js
@@ -1,34 +1,59 @@
-// search
-var search_input = document.querySelector("input[name='photographer_search_field']");
-search_input.addEventListener("keyup", (e) => {
+// function initNewEvent() {
+//   myEvent = {
+//     orginainizer: undefined,
+//     photographers: []
+//   };
+// }
+
+// variables
+let myEvent = {
+  orginainizer: undefined,
+  photographers: []
+};
+
+var new_list = document.querySelector("div[class = 'container_form']");
+var add_phot = document.querySelector("input[class='bt_add_photographer']");
+var search_input = document.querySelector(
+  "input[name='photographer_search_field']"
+);
+//all these varibles are null
+
+// Event Listeners
+// the event listener for remove photographer is already in the botton tag
+console.log(new_list);
+//search_input.addEventListener("keyup", photographer_search);
+function photographer_search() {
+  //photographer search event Listener
   var s = photographer_search_field.value;
   console.log(s);
-  doFetchRequest('GET', "user/search?name=" + s, {'Content-Type':'text/html'}, undefined)
-  .then((res) => {return res.text()})
-  .then((body) => {document.getElementById('search_result').innerHTML = body})
-})
+  doFetchRequest(
+    "GET",
+    "user/search?name=" + s,
+    { "Content-Type": "text/html" },
+    undefined
+  )
+    .then(res => {
+      return res.text();
+    })
+    .then(body => {
+      document.getElementById("search_result").innerHTML = body;
+    });
+}
 
-let myEvent
-initNewEvent()
+// search_input.addEventListener("load",);
 
 // da inizializzare ogni volta che creo una nuova galleria
-function new_photographer_list(organizzatore){
+// when page is reloaded {loggedId,}
+function new_photographer_list(organizzatore) {
   myEvent.organizer = organizzatore;
-  myEvent.photographers.push(organizzatore);
+  return myEvent.photographers.push(organizzatore);
 }
 
 function add_photographer(photographer) {
-  myEvent.photographers.push(photographer.id);
+  return myEvent.photographers.push(photographer.id);
 }
 
 function remove_photographer(photographer) {
-  let target = myEvent.photographers.findIndex(e => (photographer.id == e._id));
-  myEvent.photographers.splice(target,1);
-}
-
-function initNewEvent() {
-  myEvent = {
-    orginainizer: undefined,
-    photographers: []
-  }
+  let target = myEvent.photographers.findIndex(e => photographer.id == e._id);
+  return myEvent.photographers.splice(target, 1);
 }
