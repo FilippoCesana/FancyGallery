c9090ad048cfccea2a3b571d286f9a0b6319a3a9
Canzo97 <canzoniere70@gmail.com>
2019-12-12 14:30:29 +0100

    m

--- FancyGallery_skeleton/public/entry_points/newevent.js
+++ FancyGallery_skeleton/public/entry_points/newevent.js
@@ -9,20 +9,17 @@ function start() {
     photographers: []
   };
 
-  let search_input = document.getElementById("search_input");
-  let add_btn = document.getElementsByClassName(".plus");
-  let delete_btn = document.getElementsByClassName(".minus");
+  let plus_list = document.getElementById("plus_list");
+  let minus_list = document.getElementById("minus_list");
 
-  //var new_list = document.querySelector("div[class = 'container_form']");
-  //var add_phot = document.querySelector("input[class='bt_add_photographer']");
+  let search_input = document.getElementById("search_input");
+  let add_btn = document.getElementsByClassName("plus");
+  let delete_btn = document.getElementsByClassName("minus");
+
   //all these varibles are null
 
   // Event Listeners
-  search_input.addEventListener("keypress", photographer_search);
-  add_btn.addEventListener("onclick", add_photographer);
-  delete_btn.addEventListener("onclick", remove_photographer);
-
-  function photographer_search() {
+  search_input.addEventListener("keypress", () => {
     doFetchRequest(
       "GET",
       "user/search?name=" + search_input.value,
@@ -38,17 +35,15 @@ function start() {
       .then(body => {
         document.getElementById("search_result").innerHTML = body;
       });
-  }
+  });
 
-  // da inizializzare ogni volta che creo una nuova galleria
-  // when page is reloaded {loggedId,}
-
-  function add_photographer(myEvent, photographer) {
+  add_btn.addEventListener("onclick", (myEvent, photographer) => {
+    add_btn.parentNode.removeChild(add_btn);
     return myEvent.photographers.push(photographer.id);
-  }
+  });
 
-  function remove_photographer(myEvent, photographer) {
+  delete_btn.addEventListener("onclick", (myEvent, photographer) => {
     let target = myEvent.photographers.findIndex(e => photographer.id == e._id);
     return myEvent.photographers.splice(target, 1);
-  }
+  });
 }
