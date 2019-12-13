fc0c8ac7ea73ab049804827dca9b9a2558b41fb7
Nicola Brunner <brunnn@usi.ch>
2019-12-13 14:09:28 +0100

    route corretta a picture_upload

--- FancyGallery_skeleton/serverftp.js
+++ FancyGallery_skeleton/serverftp.js
@@ -1,3 +1,9 @@
+// Per killare il server FTP:
+// sudo killall -9 node
+
+// Per runnare tutto:
+// sudo yarn run
+
 const FtpSrv = require('ftp-srv');
 const ftpServer = new FtpSrv({
   ftp: "127.0.0.1:21",
@@ -15,6 +21,3 @@ ftpServer.listen()
 // connection.on('STOR', (error, fileName) => { console.log(fileName); });
 
 module.exports = ftpServer;
-
-// per killare il FTPserver:
-// sudo killall -9 node
