2c47d86793093895726b0c779b67ee2d04db0a1d
Nicola Brunner <brunnn@usi.ch>
2019-12-14 16:04:29 +0100

    serverftp

--- FancyGallery_skeleton/serverftp.js
+++ FancyGallery_skeleton/serverftp.js
@@ -1,15 +1,8 @@
-// Per killare il server FTP:
-// sudo killall -9 node
-
-// Per runnare tutto:
-// sudo yarn run
+const hostname = '0.0.0.0';
+const port = 21;
 
 const FtpSrv = require('ftp-srv');
-const ftpServer = new FtpSrv({
-  anonymous: true,
-  greeting: "Hello my friend",
-  tls: false
-});
+const ftpServer = new FtpSrv('ftp://' + hostname + ':' + port, {anonymous: true, greeting: "hello"});
 
 ftpServer.on('login', ({connection, username, password}, resolve, reject) => {
   console.log("new connection: ", connection);
@@ -19,7 +12,25 @@ ftpServer.on('login', ({connection, username, password}, resolve, reject) => {
   resolve ({root : "/Users/nbrunner/Documents/GitHub/FancyGallery/FancyGallery_skeleton/public/images_canon"});
   connection.on('STOR', (error, fileName) => {
     console.log("newfile: ", fileName);
-    //inviare un msg socket.io arrivato nuovo file!!
+
+    var canon_id = 098767890;
+
+    new_image = new Image();
+    new_image.src = fileName;
+    new_image.onload = drawImage;
+
+    function drawImage() {
+      var canvas = document.createElement("canvas");
+      var ctx = canvas.getContext('2d');
+      canvas.width = this.width;
+      canvas.height = this.height;
+      ctx.drawImage(this, 0,0);
+    }
+
+    var imgUrl = canvas.toDataURL();
+
+    doJSONRequest('POST', 'event/'+ canon_id +'/image', {}, {dataURL : imgUrl});
+
   });
 });
 
